bb880bc7813310b2121decb316340de648d59ce2
Connor Glosser <glosser1@gmail.com>
2015-10-01 21:48:45 -0400

    Implement prolate derivatives (possible bug in d3())

--- src/prolates.cpp
+++ src/prolates.cpp
@@ -1,67 +1,186 @@
 #include "prolates.h"
 
-using namespace std;
+namespace math = boost::math::constants;
 
-Prolate::Prolate(double w0, double timestep)
+double sinc(double);
+double sinhc(double);
+
+double d1_sinc(double);
+double d1_sinhc(double);
+
+double d2_sinc(double);
+double d2_sinhc(double);
+
+double d3_sinc(double);
+double d3_sinhc(double);
+
+Prolate::Prolate(int n)
 {
-  omega0 = w0;
-  dt = timestep;
+  width = n;
+  alpha = width*math::pi<double>();
+}
+
+double Prolate::sqrt_term(const double t)  {return std::sqrt(1 - std::pow(t/width, 2));}
+double Prolate::d1_sqrt_term(const double t) {return -t/(sqrt_term(t)*pow(width, 2));}
+double Prolate::d2_sqrt_term(const double t)
+{
+  const double q = sqrt_term(t);
+  return -std::pow(t,2)/(width*std::pow(q*width,3)) - 1/(q*std::pow(width,2));
+}
+double Prolate::d3_sqrt_term(const double t)
+{
+  const double q = sqrt_term(t);
+  return -3*t/width*(std::pow(t,2)/std::pow(q*width,5) + 1/std::pow(q*width,3));
 }
 
 //=============================================================
 
-double sinc(double t, double omega = 1)
+double Prolate::d0(const double t)
 {
-  double arg = omega*t;
-  if (abs(arg) <= TOLER) {
-    return 1 - pow(arg, 2)/6 + pow(arg, 4)/120;
+  if(std::abs(t) >= width) return 0;
+  const double pi = math::pi<double>(), q = sqrt_term(t);
+  return sinc(pi*t)*alpha*sinhc(alpha*q)/std::sinh(alpha);
+}
+
+double Prolate::d1(const double t)
+{
+  const double pi = math::pi<double>();
+
+  if(std::abs(t) >= width) {
+    return 0;
   } else {
-    return sin(arg)/arg;
+    const double q = sqrt_term(t), d1q = d1_sqrt_term(t);
+    return
+      ( //If you look closely, you'll see a product rule here.
+        alpha*pi*sinhc(alpha*q)*d1_sinc(pi*t) +
+        std::pow(alpha, 2)*d1q*sinc(pi*t)*d1_sinhc(alpha*q)
+      )/std::sinh(alpha);
   }
 }
 
-double dsinc(double t, double omega = 1)
+double Prolate::d2(const double t)
 {
-  double arg = omega*t;
-  if (abs(arg) <= TOLER) {
-    return omega*(0 - arg/3 + pow(arg, 3)/30);
+  const double pi = math::pi<double>();
+
+  if(std::abs(t) >= width) {
+    return 0;
   } else {
-    return omega*(cos(arg)/arg - sin(arg)/pow(arg, 2));
+    const double q = sqrt_term(t), d1q = d1_sqrt_term(t), d2q = d2_sqrt_term(t);
+    return
+      (
+        2*std::pow(alpha,2)*d1q*pi*d1_sinc(pi*t)*d1_sinhc(alpha*q) +
+        alpha*std::pow(pi,2)*sinhc(alpha*q)*d2_sinc(pi*t) +
+        alpha*sinc(pi*t)*(alpha*d2q*d1_sinhc(alpha*q) + std::pow(alpha*d1q,2)*d2_sinhc(alpha*q))
+      )/std::sinh(alpha);
   }
 }
 
-double ddsinc(double t, double omega = 1)
+double Prolate::d3(const double t)
 {
-  double arg = omega*t;
-  if (abs(arg) <= TOLER) {
-    return -1*pow(omega, 2)/3*(1 - 3*pow(arg, 2)/10 + pow(arg, 4)/56);
-  } else {
-    return pow(omega, 2)*
-      (-2*cos(arg)/pow(arg, 2) - sin(arg)/arg + 2*sin(arg)/pow(arg, 3));
-  }
-}
+  const double pi = math::pi<double>();
 
-double dddsinc(double t, double omega = 1)
-{
-  double arg = omega*t;
-  if (abs(arg) <= TOLER) {
-    return 0 + t*pow(omega, 4)/5 - pow(t, 3)*pow(omega, 6)/42;
+  if (std::abs(t) >= width) {
+    return 0;
   } else {
-    return pow(omega, 3)*(
-      6*cos(arg)/pow(arg, 3) -   cos(arg)/arg - 
-      6*sin(arg)/pow(arg, 4) + 3*sin(arg)/pow(arg, 2)
-    );
+    const double   q =    sqrt_term(t),  d2q = d2_sqrt_term(t);
+    const double d1q = d1_sqrt_term(t) , d3q = d3_sqrt_term(t);
+
+    double x;
+    std::cout << q << std::endl;
+    std::cout << d1q << std::endl;
+    std::cout << d2q << std::endl;
+    std::cout << d3q << std::endl;
+
+    std::cin >> x;
+
+    return (
+alpha*(3*alpha*d1q*std::pow(pi,2)*d1_sinhc(alpha*q)*d2_sinc(pi*t) +
+     3*alpha*pi*d1_sinc(pi*t)*(d2q*d1_sinhc(alpha*q) + alpha*std::pow(d1q,2)*d2_sinhc(alpha*q)) +
+     alpha*(d3q*d1_sinhc(alpha*q) + alpha*d1q*
+         (3*d2q*d2_sinhc(alpha*q) + alpha*std::pow(d1q,2)*d3_sinhc(alpha*q)))*sinc(pi*t) +
+     std::pow(pi,3)*d3_sinc(pi*t)*sinhc(alpha*q))
+      )/std::sinh(alpha);
   }
 }
 
 //=============================================================
 
-double sinhc(double t, double omega = 1)
+double sinc(const double t)
 {
-  double arg = omega*t;
-  if (abs(arg) <= TOLER) {
-    return 1 + pow(arg, 2)/6 + pow(arg, 4)/120;
+  if (abs(t) <= TOLER) {
+    return 1 - std::pow(t, 2)/6 + std::pow(t, 4)/120;
   } else {
-    return sinh(arg)/arg;
+    return sin(t)/t;
+  }
+}
+
+double sinhc(const double t)
+{
+  if (std::abs(t) <= TOLER) {
+    return 1 + std::pow(t, 2)/6 + std::pow(t, 4)/120;
+  } else {
+    return std::sinh(t)/t;
+  }
+}
+
+//=============================================================
+
+double d1_sinc(const double t)
+{
+  if (std::abs(t) <= TOLER) {
+    return -t/3 + std::pow(t, 3)/30 - std::pow(t, 5)/840;
+  } else {
+    return cos(t)/t - sin(t)/std::pow(t, 2);
+  }
+}
+
+double d1_sinhc(const double t)
+{
+  if (std::abs(t) <= TOLER) {
+    return t/3 + std::pow(t, 3)/30 + std::pow(t, 5)/840;
+  } else {
+    return cosh(t)/t - sinh(t)/std::pow(t, 2);
+  }
+}
+
+//=============================================================
+
+double d2_sinc(const double t)
+{
+  if (std::abs(t) <= TOLER) {
+    return -1.0/3 + std::pow(t,2)/10 - std::pow(t,4)/168;
+  } else {
+    return -std::sin(t)/t - 2*std::cos(t)/std::pow(t,2) + 2*std::sin(t)/pow(t,3);
+  }
+}
+
+double d2_sinhc(const double t)
+{
+  if (std::abs(t) <= TOLER) {
+    return 1.0/3 + std::pow(t,2)/10 + std::pow(t,4)/168;
+  } else {
+    return std::sinh(t)/t - 2*std::cosh(t)/std::pow(t,2) + 2*std::sinh(t)/pow(t,3);
+  }
+}
+
+//=============================================================
+
+double d3_sinc(const double t)
+{
+  if (std::abs(t) <= TOLER) {
+    return t/5 - std::pow(t,3)/42 + std::pow(t,5)/1080;
+  } else {
+    return -std::cos(t)/t + 3*std::sin(t)/std::pow(t,2) +
+            6*std::cos(t)/std::pow(t,3) - 6*std::sin(t)/std::pow(t,4);
+  }
+}
+
+double d3_sinhc(const double t)
+{
+  if (std::abs(t) <= TOLER) {
+    return t/5 + std::pow(t,3)/42 + std::pow(t,5)/1080;
+  } else {
+    return std::cos(t)/t - 3*std::sin(t)/std::pow(t,2) +
+            6*std::cos(t)/std::pow(t,3) - 6*std::sin(t)/std::pow(t,4);
   }
 }
--- src/prolates.h
+++ src/prolates.h
@@ -2,13 +2,28 @@
 #define PROLATES_H
 
 #include <cmath>
+#include <boost/math/constants/constants.hpp>
+#include <iostream>
 
 constexpr double TOLER = 1e-5;
 
 class Prolate {
-  double omega0, dt;
+  double alpha;
+  int width;
+
+  double sqrt_term(double);
+  double d1_sqrt_term(double);
+  double d2_sqrt_term(double);
+  double d3_sqrt_term(double);
+
+
  public:
-  Prolate(double, double);
+  Prolate(int);
+
+  double d0(double); //0th derivative
+  double d1(double); //1st derivative
+  double d2(double); //2nd derivative
+  double d3(double); //3rd derivative
 };
 
 #endif
