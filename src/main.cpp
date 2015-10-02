046f69990064b9773a763633ed3b79fb5fb4727f
Connor Glosser <glosser1@gmail.com>
2015-10-01 21:48:45 -0400

    Implement prolate derivatives (possible bug in d3())

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include "input.h"
 #include "universe.h"
+#include "prolates.h"
 using namespace std;
 
 int main(int argc, char *argv[]) {
@@ -14,9 +15,17 @@ int main(int argc, char *argv[]) {
     cout << "      duration: " << Universe.simulation_time << endl;
     cout << "speed of light: " << Universe.c0 << endl;
     cout << "          hbar: " << Universe.hbar << endl;
+
+    Prolate p0(5);
+
+    cout << p0.d0(0) << "\t" << p0.d0(2.2) << "\t" << p0.d0(3) << endl;
+    cout << p0.d1(1e-8) << "\t" << p0.d1(2.2) << "\t" << p0.d1(3) << endl;
+    cout << p0.d2(1e-4) << "\t" << p0.d2(2.2) << "\t" << p0.d2(3) << endl;
+    cout << p0.d3(1e-4) << "\t" << p0.d3(2.2) << "\t" << p0.d3(3) << endl;
+
   } catch(CommandLineException &e) {
     // User most likely queried for help or version info, so we can just bail out
-    return 0; 
+    return 0;
   }
   return 0;
 }
