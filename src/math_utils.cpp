ea3553a7e0cde3a7a62b3426073aeaae2a9993fd
Connor Glosser <glosser1@gmail.com>
2016-11-07 14:44:54 -0500

    Remove split_real
    Will use std::modf eventually

--- src/math_utils.cpp
+++ src/math_utils.cpp
@@ -14,11 +14,3 @@ double gaussian(const double t, const double mu, const double sigma)
 {
   return std::exp(-std::pow((t - mu)/sigma, 2)/2);
 }
-
-QuotientRemainder split_real(const double num, const double denom)
-{
-  int quotient = std::floor(num/denom);
-  double remainder = std::fmod(num, denom);
-
-  return QuotientRemainder(quotient, remainder);
-}
--- src/math_utils.h
+++ src/math_utils.h
@@ -5,10 +5,7 @@
 #include <Eigen/Dense>
 #include <utility>
 
-typedef std::pair<int, double> QuotientRemainder;
-
 Eigen::Vector3d unit_normal(const double, const double);
 double gaussian(const double, const double, const double);
-QuotientRemainder split_real(const double, const double);
 
 #endif
