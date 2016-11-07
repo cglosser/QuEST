5f2d01fa1f78409da2f01261ca89ca4030e405d0
Connor Glosser <glosser1@gmail.com>
2016-11-07 10:39:38 -0500

    Build type to hold delay quotient and remainder

--- src/math_utils.h
+++ src/math_utils.h
@@ -3,8 +3,12 @@
 
 #include <cmath>
 #include <Eigen/Dense>
+#include <utility>
+
+typedef std::pair<int, double> QuotientRemainder;
 
 Eigen::Vector3d unit_normal(const double, const double);
 double gaussian(const double, const double, const double);
+QuotientRemainder split_real(const double, const double);
 
 #endif
