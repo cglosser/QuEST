df0e96348aa28778f5564b696c542a07e4461ae4
Connor Glosser <glosser1@gmail.com>
2016-11-10 22:25:25 -0500

    Add Linspace routine to math utils

--- src/math_utils.cpp
+++ src/math_utils.cpp
@@ -1,5 +1,19 @@
 #include "math_utils.h"
 
+std::vector<double> linspace(const double low, const double high, const size_t n,
+    double * const step /*= nullptr*/)
+{
+  std::vector<double> xs(n);
+  const double dx = (high - low)/(n - 1);
+  if(step) *step = dx;
+
+  for(size_t i = 0; i < n; ++i) {
+    xs[i] = low + i*dx;
+  }
+
+  return xs;
+}
+
 Eigen::Vector3d unit_normal(double theta, double phi) {
   Eigen::Vector3d rhat(
     std::sin(theta)*std::cos(phi),
