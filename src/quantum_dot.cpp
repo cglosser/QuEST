ead17dd92d94ea642486d0d87c23e19dbe0418d1
Connor Glosser <glosser1@gmail.com>
2016-11-16 23:23:30 -0500

    Google-ify QuantumDot source

--- src/quantum_dot.cpp
+++ src/quantum_dot.cpp
@@ -1,46 +1,25 @@
 #include "quantum_dot.h"
 
 QuantumDot::QuantumDot()
-    : pos(Eigen::Vector3d(0,0,0)),
+    : pos(Eigen::Vector3d(0, 0, 0)),
       frequency(0),
-      damping(std::pair<double, double>(0,0)),
-      dipole(Eigen::Vector3d(0,0,0)) {}
+      damping(std::pair<double, double>(0, 0)),
+      dipole(Eigen::Vector3d(0, 0, 0))
+{
+}
 
-QuantumDot::QuantumDot(
-  const Eigen::Vector3d &loc,
-  const double omega,
-  const std::pair<double, double> &ts,
-  const Eigen::Vector3d &dip
-)
-    : pos(loc),
-      frequency(omega),
-      damping(ts),
-      dipole(dip)
+QuantumDot::QuantumDot(const Eigen::Vector3d &loc, const double omega,
+                       const std::pair<double, double> &ts,
+                       const Eigen::Vector3d &dip)
+    : pos(loc), frequency(omega), damping(ts), dipole(dip)
 {
   history.reserve(2048);
 }
 
 Eigen::Vector3d QuantumDot::polarization(const size_t idx) const
 {
-  assert(idx < history.size());
-  double coef = 2*history[idx](1).real();
-
-  return coef*dipole;
-}
-
-matrix_elements QuantumDot::interpolate(const UniformLagrangeSet &delay,
-    int offset = 0)
-{
-  matrix_elements result(Eigen::Vector2cd::Zero());
-
-  const size_t start = history.size() - 1 - offset;
-  assert(start - config.interpolation_order >= 0);
-
-  for(int i = 0; i <= config.interpolation_order; ++i) {
-    result += history[start - i]*delay.weights[1][config.interpolation_order - i];
-  }
-
-  return result;
+  const double coef = 2 * (history.at(idx)(1)).real();
+  return coef * dipole;
 }
 
 std::ostream &operator<<(std::ostream &os, const QuantumDot &qd)
@@ -54,9 +33,9 @@ std::ostream &operator<<(std::ostream &os, const QuantumDot &qd)
 
 std::istream &operator>>(std::istream &is, QuantumDot &qd)
 {
-  is >> qd.pos[0] >> qd.pos[1] >> qd.pos[2] >> qd.frequency
-     >> qd.damping.first >> qd.damping.second
-     >> qd.dipole[0] >> qd.dipole[1] >> qd.dipole[2];
+  is >> qd.pos[0] >> qd.pos[1] >> qd.pos[2] >>
+      qd.frequency >> qd.damping.first >> qd.damping.second >>
+      qd.dipole[0] >> qd.dipole[1] >> qd.dipole[2];
 
   return is;
 }
--- src/quantum_dot.h
+++ src/quantum_dot.h
@@ -1,14 +1,12 @@
 #ifndef QUANTUM_DOT_H
 #define QUANTUM_DOT_H
 
+#include <Eigen/Dense>
 #include <complex>
 #include <istream>
 #include <ostream>
 #include <utility>
 #include <vector>
-#include <Eigen/Dense>
-
-#include "lagrange_set.h"
 
 typedef Eigen::Vector2cd matrix_elements;
 
@@ -16,17 +14,16 @@ class QuantumDot {
  public:
   QuantumDot();
   QuantumDot(const Eigen::Vector3d &, const double,
-      const std::pair<double, double> &, const Eigen::Vector3d &);
+             const std::pair<double, double> &, const Eigen::Vector3d &);
 
   std::vector<matrix_elements> history;
 
   Eigen::Vector3d polarization(const size_t) const;
-  matrix_elements interpolate(const UniformLagrangeSet &, int);
 
   friend std::ostream &operator<<(std::ostream &, const QuantumDot &);
   friend std::istream &operator>>(std::istream &, QuantumDot &);
 
- //private:
+  // private:
   Eigen::Vector3d pos;
   double frequency;
   std::pair<double, double> damping;
