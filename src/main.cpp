0980a72ebe078b7ab2a9448363460826d5e23fb1
Connor Glosser <glosser1@gmail.com>
2016-11-20 17:57:30 -0500

    Testing current convolution

--- src/main.cpp
+++ src/main.cpp
@@ -19,19 +19,31 @@ int main(int argc, char *argv[])
   try {
     auto vm = parse_configs(argc, argv);
 
+    vector<QuantumDot> qds(2);
+    qds[0].pos = Eigen::Vector3d(0, 0, 0);
+    qds[0].dipole = Eigen::Vector3d(0, 0, 1);
+
+    qds[1].pos = Eigen::Vector3d(16, 8, 4);
+    qds[1].dipole = Eigen::Vector3d(0, 0, 1);
+
     PredictorCorrector rpc(18, 22, 3.15, 1e-12);
-    vector<QuantumDot> qds(1);
-    qds[0] = QuantumDot(Eigen::Vector3d(0,0,0), 0.05, std::pair<double, double>(10, 20), Eigen::Vector3d(0, 0, 1));
 
-    BlochSystem sys(rpc, qds, 2048);
-
-    for(int i = 0; i < 2048; ++i) sys.step();
+    BlochSystem sys(rpc, qds, config.interpolation_order, 2048);
 
     for(int i = -22; i < 2048; ++i) {
-      cout << setw(5) << i << " ";
-      cout << setprecision(12) << scientific << sys.history[0][i][0][0].real() << endl;
+      const double g = skew_gaussian((i - 1024)/256., 5);
+
+      sys.history[0][i][0][1] = g;
+      sys.history[1][i][0][1] = 0;
     }
 
+    for(int i = 0; i < 2048; ++i) {
+      sys.convolve_currents();
+      sys.now++;
+
+      cout << setprecision(12);
+      cout << i << " " << sys.rabi_freqs[0] << " " << sys.rabi_freqs[1] << endl;
+    }
 
   } catch(CommandLineException &e) {
     // User most likely queried for help or version info, so we can silently
