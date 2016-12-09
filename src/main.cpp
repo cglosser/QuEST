a0af7dfd0d842eb07da0d56d26f42e4856fb6a83
Connor Glosser <glosser1@gmail.com>
2016-12-08 19:38:35 -0500

    Test pulse evaluation

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "interaction_table.h"
 #include "lagrange_set.h"
 #include "math_utils.h"
+#include "pulse.h"
 #include "quantum_dot.h"
 
 using namespace std;
@@ -20,30 +21,18 @@ int main(int argc, char *argv[])
     cout << setprecision(12) << scientific;
     auto vm = parse_configs(argc, argv);
 
-    DotTable qds(2);
+    Pulse pulse(1, 2, 0.2, 1, Eigen::Vector3d(0,0,1), Eigen::Vector3d(2,3,4));
+    Pulse p;
 
-    const double dt = config.dt;
-    const int nsteps = 1/dt;
-    using Hist = PredictorCorrector::Integrator::HistoryArray;
+    cin >> p;
+    cout << p;
 
-    qds[0] =
-        QuantumDot(Eigen::Vector3d(0, 0, 0), 2278.9,
-                   std::pair<double, double>(10, 20), Eigen::Vector3d(0, 0, 1));
-    qds[1] =
-        QuantumDot(Eigen::Vector3d(0.01, 0.01, 0.01), 2278.9,
-                   std::pair<double, double>(10, 20), Eigen::Vector3d(0, 0, 1));
+    //for(int i = -1000; i <=1000; ++i) {
+      //const double x = i/250.;
 
-    InteractionTable itable(config.interpolation_order, qds);
-    Hist ha(boost::extents[qds.size()][Hist::extent_range(-22, nsteps)][2]);
+      //cout << x << " " << pulse(Eigen::Vector3d(0,0,x), 0).transpose() << endl;
+    //}
 
-    for(int i = -22; i < nsteps; ++i) {
-      ha[0][i][0] = matrix_elements(0, gaussian((i*dt - 0.5)/0.1));
-    }
-
-    for(int i = 0; i < nsteps; ++i) {
-      itable.convolve_currents(ha, i);
-      cout << i << " " << i*dt << " " << polarization(ha[0][i][0]) << " " << itable.convolution.at(1) << endl;
-    }
   } catch(CommandLineException &e) {
     // User most likely queried for help or version info, so we can silently
     // move on
