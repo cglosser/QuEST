32861373c9d4da38ed438f3261b6594937ee637f
Connor Glosser <glosser1@gmail.com>
2016-12-13 18:00:22 -0500

    (WIP) Test 2-dot system

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,28 @@ using namespace std;
 int main(int argc, char *argv[])
 {
   try {
-    cout << setprecision(12) << scientific;
     auto vm = parse_configs(argc, argv);
 
+    shared_ptr<DotVector> dots(new DotVector(import_dots(config.qd_path)));
+    shared_ptr<Pulse> pulse(new Pulse(15589.2260227, 5, 2278.9013, 2278.9013,
+                                      Eigen::Vector3d(0, 0, 1),
+                                      Eigen::Vector3d(1, 0, 0)));
+    InteractionTable interaction_table(config.interpolation_order, dots, pulse);
+    auto superoperators(rhs_functions(*dots));
+
+    const int num_steps =
+        static_cast<int>(ceil(config.simulation_time / config.dt));
+    PredictorCorrector::Integrator solver(2, num_steps, config.dt, 18, 22, 3.15,
+                                          std::move(superoperators),
+                                          interaction_table);
+
+    ofstream bloch("bloch.dat");
+    bloch << scientific << setprecision(6);
+
+    for(int i = 1; i < num_steps; ++i) {
+      solver.solve(i);
+      bloch << i*config.dt << " " << solver.history[0][i][0].transpose() << " " << solver.history[1][i][0].transpose() << endl;
+    }
 
   } catch(CommandLineException &e) {
     // User most likely queried for help or version info, so we can silently
