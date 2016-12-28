328a6864c533e05727f106edd93ba041076c1395
Connor Glosser <glosser1@gmail.com>
2016-12-28 13:37:04 -0500

    Test history interaction

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "configuration.h"
 #include "integrator.h"
 #include "interactions/interaction_table.h"
+#include "interactions/history_interaction.h"
 #include "lagrange_set.h"
 #include "math_utils.h"
 #include "pulse.h"
@@ -15,9 +16,42 @@
 
 using namespace std;
 
+void test_history_interaction(int argc, char *argv[])
+{
+  cout << setprecision(15) << scientific;
+  auto vm = parse_configs(argc, argv);
+  auto dots = make_shared<DotVector>(import_dots(config.qd_path));
+
+  const int num_steps = 100000;
+  auto history = make_shared<HistoryArray>(boost::extents[2][num_steps][2]);
+
+  cout << "Distance (dimensionless): "
+       << ((*dots)[1].position() - (*dots)[0].position()).norm() /
+              (config.c0 * config.dt) << endl;
+
+  for(int time_idx = 0; time_idx < num_steps; time_idx++) {
+    const double time = time_idx * config.dt;
+    const double signal = gaussian((time - 5)/1);
+    (*history)[0][time_idx][0] = Eigen::Vector2cd(0, signal);
+  }
+
+  HistoryInteraction hint(dots, history, config.interpolation_order);
+
+  for(int time_idx = 10; time_idx < num_steps; time_idx++) {
+    const double time = time_idx * config.dt;
+    hint.evaluate(time_idx);
+    const double val = hint.result(1);
+    (*history)[1][time_idx][1] = Eigen::Vector2cd(val, 0);
+
+    cout << time << " " << (*history)[0][time_idx][0][1].real() << " "
+         << (*history)[1][time_idx][1][0].real() << endl;
+  }
+}
+
 int main(int argc, char *argv[])
 {
   try {
+    test_history_interaction(argc, argv);
     auto vm = parse_configs(argc, argv);
 
     shared_ptr<DotVector> dots(new DotVector(import_dots(config.qd_path)));
