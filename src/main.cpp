8e69dc018ce3d893c328b7d332f6fb93f3f5524a
Connor Glosser <glosser1@gmail.com>
2017-01-18 15:30:34 -0500

    Use Bohr radius to test dots

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,17 @@ using namespace std;
 int main(int argc, char *argv[])
 {
   try {
-    cout << setprecision(12);
+    cout << setprecision(12) << scientific;
     auto vm = parse_configs(argc, argv);
 
 
     auto qds(make_shared<DotVector>(2));
     (*qds)[0] =
         QuantumDot(Eigen::Vector3d(0, 0, 0), 2278.9013,
-                   std::pair<double, double>(10, 10), Eigen::Vector3d(1, 0, 0));
+                   std::pair<double, double>(10, 10), Eigen::Vector3d(5.2917721e-4, 0, 0));
     (*qds)[1] =
         QuantumDot(Eigen::Vector3d(0, 0, 0.01), 2278.9013,
-                   std::pair<double, double>(10, 10), Eigen::Vector3d(1, 0, 0));
+                   std::pair<double, double>(10, 10), Eigen::Vector3d(5.2917721e-4, 0, 0));
 
     auto hist(make_shared<History::HistoryArray>());
 
