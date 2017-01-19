4b9d0211aa1545a630ac1cfe04d5d75f6df6a700
Connor Glosser <glosser1@gmail.com>
2017-01-19 17:13:10 -0500

    Test rotating frame Green's function

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@
 #include <vector>
 
 #include "configuration.h"
-#include "interactions/rotating_green_function.h"
 #include "interactions/history_interaction.h"
-#include "quantum_dot.h"
+#include "interactions/rotating_green_function.h"
+#include "math_utils.h"
 
 using namespace std;
 
@@ -18,23 +18,46 @@ int main(int argc, char *argv[])
     cout << setprecision(12) << scientific;
     auto vm = parse_configs(argc, argv);
 
-    auto qds(make_shared<DotVector>(2));
+    auto qds(make_shared<DotVector>(3));
     (*qds)[0] = QuantumDot(Eigen::Vector3d(0, 0, 0), 2278.9013,
                            std::pair<double, double>(10, 10),
                            Eigen::Vector3d(5.2917721e-4, 0, 0));
     (*qds)[1] = QuantumDot(Eigen::Vector3d(0, 0, 0.01), 2278.9013,
                            std::pair<double, double>(10, 10),
                            Eigen::Vector3d(5.2917721e-4, 0, 0));
+    (*qds)[2] = QuantumDot(Eigen::Vector3d(0, 0.01, 0.01), 2278.9013,
+                           std::pair<double, double>(10, 10),
+                           Eigen::Vector3d(5.2917721e-4, 0, 0));
 
-    auto hist(make_shared<History::HistoryArray>());
+    // Set up a dummy Gaussian history
+    auto history(make_shared<History::HistoryArray>(boost::extents[3][1000][2]));
+    std::fill(history->data(), history->data() + history->num_elements(), Eigen::Vector2cd::Zero());
+    for(int i = 0; i < 1000; ++i) {
+      const double time = i * config.dt;
+      (*history)[0][i][0] =
+          Eigen::Vector2cd(0, gaussian((time - config.simulation_time / 2) /
+                                       (config.simulation_time / 10)));
+    }
 
+    ofstream ipol("input_polarization.dat");
+    ipol << scientific << setprecision(12);
+    for(int i = 0; i < 1000; ++i) {
+      ipol << i * config.dt << " " << polarization((*history)[0][i][0]) << endl;
+    }
+
+    // Initalize an interaction
     GreenFunction::RotatingDyadic dy(config.mu0, config.c0, config.omega);
+    HistoryInteraction history_interaction(qds, history,
+                                           config.interpolation_order, dy);
 
-    HistoryInteraction hits(qds, hist, config.interpolation_order, dy);
+    ofstream fd("data.dat");
+    fd << scientific << setprecision(12);
+    for(int i = 0; i < 1000; ++i) {
+      history_interaction.evaluate(i);
 
-    cout << hits.floor_delays[0] << endl;
-    for(int i = 0; i <= 3; ++i) {
-      cout << "    " << hits.coefficients[0][i] << endl;
+      fd << i * config.dt << " " << history_interaction.result(0) << " "
+         << history_interaction.result(1) << " "
+         << history_interaction.result(2) << endl;
     }
 
   } catch(CommandLineException &e) {
