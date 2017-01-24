cb9e855be2a958bb6bc6d14d8352a8856e7f28b1
Connor Glosser <glosser1@gmail.com>
2017-01-23 19:44:15 -0500

    Test sim against 2pc fortran code

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@
 #include "configuration.h"
 #include "history.h"
 #include "integrator.h"
+#include "interactions/history_interaction.h"
 #include "interactions/pulse_interaction.h"
-#include "pulse.h"
+#include "interactions/rotating_green_function.h"
 
 using namespace std;
 
@@ -19,49 +20,62 @@ int main(int argc, char *argv[])
     cout << setprecision(12) << scientific;
     auto vm = parse_configs(argc, argv);
 
-    auto qds(make_shared<DotVector>(config.num_particles));
-    (*qds)[0] = QuantumDot(Eigen::Vector3d(0, 0, 0), 2278.9013,
-                           std::pair<double, double>(10, 10),
-                           Eigen::Vector3d(5.2917721e-4, 0, 0));
-    auto rhs_funs(rhs_functions(*qds, config.omega));
 
-    constexpr int tmax = 2000;
+    cout << "Initializing..." << endl;
+
+    auto qds = make_shared<DotVector>(import_dots(config.qd_path));
+    qds->resize(config.num_particles);
+    auto rhs_funs = rhs_functions(*qds, config.omega);
 
     // Set up History
-    auto history(History::make_shared_history(config.num_particles, 22, tmax));
+    auto history(History::make_shared_history(config.num_particles, 22, config.num_timesteps));
     for(int t = -22; t <= 0 ; ++t) {
-      (*history)[0][t][0] = Eigen::Vector2cd(1, 0); // Ground state
+      for(int sol_idx = 0; sol_idx < config.num_particles; ++sol_idx) {
+        (*history)[sol_idx][t][0] = Eigen::Vector2cd(1, 0); // Ground state
+      }
     }
 
     // Set up Pulse and Interaction
     auto pulse1(
-        make_shared<Pulse>(1558.92260227, 5, config.omega, config.omega,
+        make_shared<Pulse>(0.8*15589.2260227, 0.5, config.omega, 10*config.omega,
                            Eigen::Vector3d(0, 0, config.omega / config.c0),
                            Eigen::Vector3d(1, 0, 0)));
 
-    auto pulse2(
-        make_shared<Pulse>(1558.92260227, 10, config.omega, config.omega,
-                           Eigen::Vector3d(0, 0, config.omega / config.c0),
-                           Eigen::Vector3d(1, 0, 0)));
+    auto rotating_dyadic(make_shared<GreenFunction::RotatingDyadic>(
+        config.mu0, config.c0, config.hbar, config.omega));
 
+    auto hits = make_shared<HistoryInteraction>(qds, history, rotating_dyadic, config.interpolation_order);
 
     std::vector<std::shared_ptr<Interaction>> interactions{
-      static_pointer_cast<Interaction>(make_shared<PulseInteraction>(qds, pulse1)),
-      static_pointer_cast<Interaction>(make_shared<PulseInteraction>(qds, pulse2))
+        static_pointer_cast<Interaction>(make_shared<PulseInteraction>(qds, pulse1)),
+        static_pointer_cast<Interaction>(hits)
     };
 
     // Set up Integrator
     PredictorCorrector::Integrator integrator(config.dt, 18, 22, 3.15, history,
                                               rhs_funs, std::move(interactions));
 
+    cout << "Solving..." << endl;
+
     // Solve the system
-    for(int i = 0; i < tmax; ++i) {
+    for(int i = 0; i < config.num_timesteps; ++i) {
+      cout << "  " << i << endl;
       integrator.solve(i);
+
+
     }
 
-    for(int t = 0; t < tmax; ++t) {
+    cout << "Writing output..." << endl;
+
+    ofstream output("output.dat");
+    output << setprecision(12) << scientific;
+    for(int t = 0; t < config.num_timesteps; ++t) {
       const double time = t * config.dt;
-      cout << time << " " << (*history)[0][t][0].transpose() << endl;
+      output << time << " ";
+      for(int sol_idx = 0; sol_idx < config.num_particles; ++sol_idx) {
+        output << (*history)[sol_idx][t][0].transpose() << " ";
+      }
+      output << endl;
     }
   } catch(CommandLineException &e) {
     // User most likely queried for help or version info, so we can silently
