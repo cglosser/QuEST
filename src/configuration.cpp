c6f6a67d6538a5d4a36ce29f21997c63ed2c75e8
Connor Glosser <glosser1@gmail.com>
2017-01-25 08:46:41 -0500

    Split config sections into separate options_descriptions
    If the need arises, this should make it easier to break out these options
    into their respective modules.

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -14,33 +14,41 @@ po::variables_map parse_configs(int argc, char *argv[]) {
     ("config,c", po::value<string>(&config_path)->default_value("input.cfg"), "path to configuration file")
   ;
 
-  po::options_description file_description("System parameters");
-  file_description.add_options()
-    ("files.qd_path",    po::value<std::string>(&config.qd_path)->default_value("dots.dat"), "path to dot configuration file")
+  po::options_description files_description("Configuration files");
+  files_description.add_options()
+    ("files.qd_path",    po::value<std::string>(&config.qd_path)->default_value("dots.cfg"), "path to dot configuration file")
     ("files.pulse_path", po::value<std::string>(&config.pulse_path)->default_value("pulse.cfg"), "path to pulse configuration file")
+  ;
 
-    ("parameters.num_particles",       po::value<int>(&config.num_particles)->required(), "number of particles in the system")
-    ("parameters.num_timesteps",       po::value<int>(&config.num_timesteps)->required(), "number of solution timesteps")
-    ("parameters.timestep",            po::value<double>(&config.dt)->required(), "timestep size")
-    ("parameters.interpolation_order", po::value<int>(&config.interpolation_order)->required(), "order of the interpolants")
-
+  po::options_description constants_description("Physical constants");
+  constants_description.add_options()
     ("constants.c0",   po::value<double>(&config.c0)->required(), "speed of light in vacuum")
     ("constants.hbar", po::value<double>(&config.hbar)->required(), "reduced Planck constant")
     ("constants.mu0",  po::value<double>(&config.mu0)->required(), "vacuum permeability")
     ("constants.laser_frequency",  po::value<double>(&config.omega)->required(), "(angular) frequency of incident pulse")
   ;
 
+  po::options_description parameters_description("System parameters");
+  parameters_description.add_options()
+    ("parameters.num_particles",       po::value<int>(&config.num_particles)->required(), "number of particles in the system")
+    ("parameters.num_timesteps",       po::value<int>(&config.num_timesteps)->required(), "number of solution timesteps")
+    ("parameters.timestep",            po::value<double>(&config.dt)->required(), "timestep size")
+    ("parameters.interpolation_order", po::value<int>(&config.interpolation_order)->required(), "order of the interpolants")
+  ;
+
   po::options_description cmdline_options, file_options;
   cmdline_options.add(cmd_line_description);
-  file_options.add(file_description);
+  file_options.add(files_description)
+      .add(constants_description)
+      .add(parameters_description);
 
   po::variables_map vm;
   po::store(po::command_line_parser(argc, argv).options(cmdline_options).run(), vm);
   po::notify(vm);
 
   if (vm.count("help")) {
-    po::options_description visible("EMRG options");
-    visible.add(cmd_line_description).add(file_description);
+    po::options_description visible("QuEST options");
+    visible.add(cmd_line_description).add(file_options);
     cout << visible << "\n";
     throw CommandLineException();
   }
