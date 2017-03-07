79077a64e17d3bf7b82a6f34d0dad20995f90e0e
Connor Glosser <glosser1@gmail.com>
2017-03-07 09:28:40 -0500

    Use tree-based analysis to find interesting curves

--- tools/find_interesting.py
+++ tools/find_interesting.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from itertools import chain
+from itertools import chain, zip_longest
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
 
 lo_alpha, hi_alpha = 0.075, 1
 
-path = "/home/connor/Scratch/neighborhood/full_neighborhood/sim/neighborhood_00/output.dat"
+path = "/home/connor/Scratch/neighborhood/full_neighborhood/sim/neighborhood_03/output.dat"
 
 output = np.loadtxt(path)
 
@@ -13,23 +19,37 @@ pop = output[::3,1::4]
 
 lines = []
 for i in range(1024):
-    lines.append(plt.plot(times, pop[:,i], alpha=lo_alpha))
+    lines.append(plt.plot(times, pop[:,i], alpha=lo_alpha, zorder=1024 - i))
 
 plt.show(block=False)
 
 
 interesting_idx = set()
 
-for idx, line in enumerate(chain.from_iterable(lines)):
-    line.set_alpha(hi_alpha)
+for group in grouper(enumerate(chain.from_iterable(lines)), 10):
+    for idx, line in group:
+        line.set_alpha(hi_alpha)
+
     plt.draw()
 
-    query = input("Interesting ({})? ".format(idx))
+    query = input("Any interesting?")
     if query.lower() in ['y', 'yes']:
-        interesting_idx.add(idx)
-        line.set_alpha(lo_alpha)
+        for idx, line in group:
+            line.set_alpha(lo_alpha)
+
+        for idx, line in group:
+            line.set_alpha(hi_alpha)
+            plt.draw()
+
+            query = input("Interesting ({})? ".format(idx))
+            if query.lower() in ['y', 'yes']:
+                interesting_idx.add(idx)
+                line.set_alpha(lo_alpha)
+            else:
+                line.remove()
     else:
-        line.remove()
+        for idx, line in group:
+            line.remove()
 
 
 with open("interesting.dat", 'w') as f:
