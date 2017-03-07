02c2608c7363f721601f78b46747f58c4df01c3f
Connor Glosser <glosser1@gmail.com>
2017-03-07 08:26:56 -0500

    Update find_interesting to record interesting curves

--- tools/find_interesting.py
+++ tools/find_interesting.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from itertools import chain
 
-lo_alpha, hi_alpha = 0.05, 1
+lo_alpha, hi_alpha = 0.075, 1
 
 path = "/home/connor/Scratch/neighborhood/full_neighborhood/sim/neighborhood_00/output.dat"
 
 output = np.loadtxt(path)
 
-times = output[:,0]
-pop = output[:,1::3]
+times = output[::3,0]
+pop = output[::3,1::4]
 
 lines = []
 for i in range(1024):
@@ -17,10 +17,21 @@ for i in range(1024):
 
 plt.show(block=False)
 
-for line in chain.from_iterable(lines):
+
+interesting_idx = set()
+
+for idx, line in enumerate(chain.from_iterable(lines)):
     line.set_alpha(hi_alpha)
     plt.draw()
 
-    input("Interesting? ")
+    query = input("Interesting ({})? ".format(idx))
+    if query.lower() in ['y', 'yes']:
+        interesting_idx.add(idx)
+        line.set_alpha(lo_alpha)
+    else:
+        line.remove()
 
-    line.set_alpha(lo_alpha)
+
+with open("interesting.dat", 'w') as f:
+    for idx in interesting_idx:
+        f.write(str(idx) + "\n")
