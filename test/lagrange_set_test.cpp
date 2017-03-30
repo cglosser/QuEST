cc92c712dfd264e744ae4e8ec21dece146a1b0b6
Connor Glosser <glosser1@gmail.com>
2017-03-30 17:25:05 -0400

    Rudimentary test of interpolation table construction

---
+++ test/lagrange_set_test.cpp
@@ -0,0 +1,20 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Lagrange set"
+#include <boost/test/unit_test.hpp>
+
+#include "../src/lagrange_set.h"
+
+BOOST_AUTO_TEST_CASE(table)
+{
+  constexpr int order = 5;
+  Interpolation::UniformLagrangeSet ULS_easy(order);
+
+  BOOST_CHECK(ULS_easy.weights.shape()[0] == Interpolation::NUM_DERIVATIVES);
+  BOOST_CHECK(ULS_easy.weights.shape()[1] == order + 1);
+
+  constexpr double eval_point = 0.5;
+  Interpolation::UniformLagrangeSet ULS_full(eval_point, order);
+
+  BOOST_CHECK(ULS_full.weights.shape()[0] == Interpolation::NUM_DERIVATIVES);
+  BOOST_CHECK(ULS_full.weights.shape()[1] == order + 1);
+}
