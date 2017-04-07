bbd2a736c7217971a7758193f4cf80a21ad14bf5
Connor Glosser <glosser1@gmail.com>
2017-04-06 20:55:42 -0400

    Fix version string
    Turns out, __TIMESTAMP__ just pulls the timestamp from the current file
    and has nothing to do with the time at compilation. __TIME__ and __DATE__
    work much better.

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -55,7 +55,8 @@ po::variables_map parse_configs(int argc, char *argv[]) {
 
   if (vm.count("version")) {
     cout << "Quantum Electromagnetics Simulation Toolbox (QuEST), version 0" << endl;
-    cout << "Compiled with " << __VERSION__ << " on " << __TIMESTAMP__ << endl;
+    cout << "Compiled with " << __VERSION__ << " on " << __DATE__ << " ("
+         << __TIME__ << ")" << endl;
     throw CommandLineException();
   }
 
