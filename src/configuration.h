61dd1f1401deca8605dbbaa69f6b9208d0f7356f
Connor Glosser <glosser1@gmail.com>
2017-04-20 12:24:26 -0400

    Rework input file to specify total simulation time
    This is a much more experimentally-relevant parameter, so I think it's
    easier to calculate the number of timesteps from this rather than the other
    way around.

--- src/configuration.h
+++ src/configuration.h
@@ -2,17 +2,18 @@
 #define CONFIG_H
 
 #include <boost/program_options.hpp>
+#include <cmath>
 #include <exception>
 #include <fstream>
 #include <iostream>
 #include <iterator>
 #include <string>
 
-struct Configuration{
+struct Configuration {
   int num_particles;
   int num_timesteps;
   double c0, hbar, mu0, omega;
-  double dt;
+  double dt, total_time;
   int interpolation_order;
   std::string qd_path, pulse_path;
 };
@@ -21,11 +22,10 @@ struct Configuration{
 // to main() in an atypical manner (--help or --version flag, config
 // file not found, etc.)
 struct CommandLineException : public std::exception {
-  const char *what() const throw() {return "Have a nice day! :)";}
+  const char *what() const throw() { return "Have a nice day! :)"; }
 };
 
 boost::program_options::variables_map parse_configs(int argc, char *argv[]);
-void populate_universe(boost::program_options::variables_map const &);
 
 extern Configuration config;
 
