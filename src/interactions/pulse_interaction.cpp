32729003ac9d55daccb59b43bcd80c5f3982d88c
Jack Hamel <jackhaml16@gmail.com>
2017-05-30 13:13:43 -0400

    Reformatted pulse_interactions to include dt in initialization line rather than inside the evaluate function.

--- src/interactions/pulse_interaction.cpp
+++ src/interactions/pulse_interaction.cpp
@@ -2,14 +2,14 @@
 
 PulseInteraction::PulseInteraction(const std::shared_ptr<const DotVector> &dots,
                                    const std::shared_ptr<const Pulse> pulse,
-                                   const double x)
-    : Interaction(dots), pulse(std::move(pulse)), hbar(x)
+                                   const double hbar, const double dt)
+    : Interaction(dots), pulse(std::move(pulse)), hbar(hbar), dt(dt)
 {
 }
 
 const Interaction::ResultArray &PulseInteraction::evaluate(const int time_idx)
 {
-  const double time = time_idx * config.dt;
+  const double time = time_idx * dt;
 
   for(size_t i = 0; i < dots->size(); ++i) {
     results[i] =
