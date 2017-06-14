f30792a4ae9d5d0222488ad8aa02e7523b08c274
Jack Hamel <jackhaml16@gmail.com>
2017-06-14 16:19:53 -0400

    added analytic values to history_interaction_test and used boost to compare them to sim results. Changed a path in CMakelists in test dir.

--- test/history_interaction_test.cpp
+++ test/history_interaction_test.cpp
@@ -1,6 +1,5 @@
 #include "../src/interactions/history_interaction.h"
 #include <boost/test/unit_test.hpp>
-#include <iostream>
 
 BOOST_AUTO_TEST_SUITE(history_interaction)
 
@@ -21,7 +20,7 @@ BOOST_AUTO_TEST_CASE(history_interaction)
   auto history(History::make_shared_history(2, 22, 1));
   for(int dot_idx=0; dot_idx<2; ++dot_idx) {
     for(int time_idx=-22; time_idx<=0; ++time_idx) {
-      (*history)[dot_idx][time_idx][0] = History::soltype(1, 0);
+      (*history)[dot_idx][time_idx][0] = History::soltype(1, 1);
     }
   }
 
@@ -30,7 +29,10 @@ BOOST_AUTO_TEST_CASE(history_interaction)
   auto hist_inter = HistoryInteraction(dots, history, dyadic, 1, 1, 1);
 
   auto result = hist_inter.evaluate(1);
-  std::cout << result << std::endl;
+  const std::vector<double> compare_array = {-2.6953857109, -2.6953857109};
+
+  BOOST_CHECK_CLOSE(result(0).real(),compare_array.at(0),1e-6);
+  BOOST_CHECK_CLOSE(result(1).real(),compare_array.at(1),1e-6);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
