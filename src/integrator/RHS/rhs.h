14ddd3da4439ed4a188c4a9a40d5c757f2e56ad9
Connor Glosser <glosser1@gmail.com>
2017-06-26 12:25:31 -0400

    Revert "Pass array of rhs funcs to RHS() objects"
    This reverts commit d6be3a9c284ac28aad29610b5e62ee38c48a38f3.

--- src/integrator/RHS/rhs.h
+++ src/integrator/RHS/rhs.h
@@ -1,9 +1,7 @@
 #ifndef RHS_H
 #define RHS_H
 
-#include <functional>
 #include <memory>
-#include <vector>
 #include "../history.h"
 
 namespace Integrator {
@@ -14,24 +12,13 @@ namespace Integrator {
 template <class soltype>
 class Integrator::RHS {
  public:
-  RHS(const double dt, const std::shared_ptr<History<soltype>> &history,
-      const std::vector<std::function<soltype(soltype, double)>> &rhs_functions)
-      : dt(dt), history(history), rhs_functions(rhs_functions){};
-  void evaluate(const int) const;
+  RHS(const double dt, const std::shared_ptr<History<soltype>> &history)
+      : dt(dt), history(history){};
+  virtual void evaluate(const int) const = 0;
 
  protected:
   double dt;
   std::shared_ptr<History<soltype>> history;
-  std::vector<std::function<double(soltype, double)>> rhs_functions;
 };
-
-template <class soltype>
-void Integrator::RHS<soltype>::evaluate(const int n) const
-{
-  const double time = n * dt;
-  for(int i = 0; i < static_cast<int>(history->array.shape()[0]); ++i) {
-    history->array[i][n][1] = rhs_functions[i](history->array[i][n][0], time);
-  }
-}
 
 #endif
