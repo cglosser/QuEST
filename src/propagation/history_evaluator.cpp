29ea0ad1817ebf0e74cc15411fe60b3ed08bb921
Connor Glosser <glosser1@gmail.com>
2017-06-29 13:34:14 -0400

    Remove "propagation" dir
    This stuff was an attempt to reconcile the differences between Bloch & LLG
    stuff, but, for the moment, I think they are irreconcilable. So we'll stick
    with known working code elsewhere until I can think of a better way to patch
    things together.

--- src/propagation/history_evaluator.cpp
+++
@@ -1,9 +0,0 @@
-#include "history_evaluator.h"
-
-template <class scalarFieldType, class soltype>
-HistoryEvaluator::HistoryEvaluator(
-    const std::shared_ptr<const Integrator::History<soltype>> &history,
-    const std::shared_ptr<GreenFunction::Dyadic> &dyad, const int interp_order,
-    const double c0, const double dt) :
-      FieldEvaluator<scalarFieldType>(dots), history(history) 
-{}
