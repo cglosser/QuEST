80b81caf3585f2d95075e0db4b41e667f1ce2ad8
Jack Hamel <jackhaml16@gmail.com>
2017-07-10 15:23:50 -0400

    removed many unecessary files and removed outdated/incomplete tests

---
+++ src/integrator/RHS/llg_rhs.cpp
@@ -0,0 +1,29 @@
+#include "llg_rhs.h"
+#include <ostream>
+#include <fstream>
+#include <string>
+
+Integrator::LLG_RHS::LLG_RHS(
+    const double dt,
+    const std::shared_ptr<Integrator::History<soltype>> &history,
+    std::vector<std::shared_ptr<Interaction>> interactions,
+    rhs_func_vector rhs_functions)
+    : Integrator::RHS<soltype>(dt, history),
+      num_solutions(history->array.shape()[0]),
+      interactions(std::move(interactions)),
+      rhs_functions(std::move(rhs_functions))
+{
+}
+
+void Integrator::LLG_RHS::evaluate(const int step) const
+{
+  auto pulse_interactions = interactions[0]->evaluate(step);
+  auto history_interactions = interactions[1]->evaluate(step);
+
+  for(int sol = 0; sol < num_solutions; ++sol) {
+    history->array[sol][step][1] =
+        rhs_functions[sol](history->array[sol][step][0],
+                      pulse_interactions[sol] + history_interactions[sol]);
+    std::cout << history->array[sol][step][1].transpose() << std::endl;
+  }
+}
