bd5c3506db1204fb60940a6263d0b663787bf489
Connor Glosser <glosser1@gmail.com>
2017-08-17 12:24:11 -0400

    Prepping to abstract a "HistoryInteraction"
    Both the AIM and Direct field calculations could be classified as a
    "history interaction," so I feel as though the commonalities should come out
    into their own virtual class.

---
+++ src/interactions/direct_interaction.cpp
@@ -0,0 +1,86 @@
+#include "history_interaction.h"
+
+using Vec3d = Eigen::Vector3d;
+
+HistoryInteraction::HistoryInteraction(
+    const std::shared_ptr<const DotVector> &dots,
+    const std::shared_ptr<const Integrator::History<Eigen::Vector2cd>> &history,
+    const std::shared_ptr<Propagation::RotatingFramePropagator> &dyadic,
+    const int interp_order, const double c0, const double dt)
+    : Interaction(dots, dt),
+      history(history),
+      dyadic(dyadic),
+      interp_order(interp_order),
+      num_interactions(dots->size() * (dots->size() - 1) / 2),
+      floor_delays(num_interactions),
+      coefficients(boost::extents[num_interactions][interp_order + 1]),
+      c0(c0)
+{
+  build_coefficient_table();
+}
+
+void HistoryInteraction::build_coefficient_table()
+{
+  Interpolation::UniformLagrangeSet lagrange(interp_order);
+
+  for(int pair_idx = 0; pair_idx < num_interactions; ++pair_idx) {
+    int src, obs;
+    std::tie(src, obs) = idx2coord(pair_idx);
+
+    Vec3d dr(separation((*dots)[src], (*dots)[obs]));
+
+    std::pair<int, double> delay(split_double(dr.norm() / (c0 * dt)));
+
+    floor_delays[pair_idx] = delay.first;
+    lagrange.evaluate_derivative_table_at_x(delay.second, dt);
+
+    std::vector<Eigen::Matrix3cd> interp_dyads(
+        dyadic->coefficients(dr, lagrange));
+
+    for(int i = 0; i <= interp_order; ++i) {
+      coefficients[pair_idx][i] =
+          (*dots)[obs].dipole().dot(interp_dyads[i] * (*dots)[src].dipole());
+    }
+  }
+}
+
+const Interaction::ResultArray &HistoryInteraction::evaluate(const int time_idx)
+{
+  results.setZero();
+
+  for(int pair_idx = 0; pair_idx < num_interactions; ++pair_idx) {
+    int src, obs;
+    std::tie(src, obs) = idx2coord(pair_idx);
+    const int s = time_idx - floor_delays[pair_idx];
+
+    for(int i = 0; i <= interp_order; ++i) {
+      if(s - i < history->array.index_bases()[1]) continue;
+
+      constexpr int RHO_01 = 1;
+
+      results[src] +=
+          (history->array[obs][s - i][0])[RHO_01] * coefficients[pair_idx][i];
+      results[obs] +=
+          (history->array[src][s - i][0])[RHO_01] * coefficients[pair_idx][i];
+    }
+  }
+
+  return results;
+}
+
+int HistoryInteraction::coord2idx(int row, int col)
+{
+  assert(row != col);
+  if(col > row) std::swap(row, col);
+
+  return row * (row - 1) / 2 + col;
+}
+
+std::pair<int, int> HistoryInteraction::idx2coord(const int idx)
+{
+  const int row = std::floor((std::sqrt(1 + 8 * idx) + 1) / 2.0);
+  const int col = idx - row * (row - 1) / 2;
+
+  return std::pair<int, int>(row, col);
+}
+
---
+++ src/interactions/direct_interaction.h
@@ -0,0 +1,38 @@
+#ifndef HISTORY_INTERACTION_H
+#define HISTORY_INTERACTION_H
+
+#include <Eigen/Dense>
+#include <boost/multi_array.hpp>
+
+#include "../math_utils.h"
+#include "../integrator/history.h"
+#include "../lagrange_set.h"
+#include "../quantum_dot.h"
+#include "green_function.h"
+#include "interaction.h"
+
+class HistoryInteraction : public Interaction {
+ public:
+  HistoryInteraction(
+      const std::shared_ptr<const DotVector> &,
+      const std::shared_ptr<const Integrator::History<Eigen::Vector2cd>> &,
+      const std::shared_ptr<Propagation::RotatingFramePropagator> &, const int,
+      const double, const double);
+
+  virtual const ResultArray &evaluate(const int);
+
+ private:
+  std::shared_ptr<const Integrator::History<Eigen::Vector2cd>> history;
+  std::shared_ptr<Propagation::RotatingFramePropagator> dyadic;
+  int interp_order, num_interactions;
+  std::vector<int> floor_delays;
+  boost::multi_array<cmplx, 2> coefficients;
+  const double c0;
+
+  void build_coefficient_table();
+
+  static int coord2idx(int, int);
+  static std::pair<int, int> idx2coord(const int);
+};
+
+#endif
