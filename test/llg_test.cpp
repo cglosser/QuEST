6451c54a62d6ec77ca47eb07dce440a545f60b6c
Jack Hamel <jackhaml16@gmail.com>
2017-10-17 16:23:17 -0400

    corrected a few errors in llg_test.cpp

--- test/llg_test.cpp
+++ test/llg_test.cpp
@@ -6,14 +6,18 @@ BOOST_AUTO_TEST_SUITE(llg_rhs)
 
 BOOST_AUTO_TEST_CASE(llg_rhs)
 {
-  typedef Eigen : Vector3d vec3d;
+  typedef Eigen::Vector3d vec3d;
 
-  vec3d pos(1, 1, 1);
-  vec3d mag(100, 100, 100);
+  vec3d pos(1, 2, 1);
+  const vec3d mag(100, 100, 100);
   const double alpha = 2;
   const double gamma0 = 3;
   const double sat_mag = 100;
 
   MagneticParticle mp(pos, alpha, gamma0, sat_mag, mag);
+  const vec3d field(10,100,50);
+
+  vec3d mp.llg_rhs(mag, field);
+
 }
 BOOST_AUTO_TEST_SUITE_END()
