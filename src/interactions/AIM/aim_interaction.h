bd74a7b37c046a93010664ff62f1e6fc5f889775
Connor Glosser <glosser1@gmail.com>
2018-02-08 16:31:43 -0500

    Move normalizations into separate header
    Going to be used in multiple places
    
    ACTUALLY move the normalization stuff

--- src/interactions/AIM/aim_interaction.h
+++ src/interactions/AIM/aim_interaction.h
@@ -12,42 +12,11 @@
 #include "fourier.h"
 #include "grid.h"
 #include "interactions/history_interaction.h"
+#include "normalization.h"
 #include "spacetime.h"
 
 namespace AIM {
   class AimInteraction;
-
-  namespace normalization {
-    using SpatialNorm = std::function<cmplx(const Eigen::Vector3d &)>;
-    const SpatialNorm unit = [](__attribute__((unused))
-                                const Eigen::Vector3d &v) { return 1; };
-
-    class Laplace {
-     public:
-      Laplace(const double alpha = 1) : alpha(alpha){};
-      double operator()(const Eigen::Vector3d &dr) const
-      {
-        return 1 / (alpha * dr.norm());
-      }
-
-     private:
-      double alpha;
-    };
-
-    class Helmholtz {
-     public:
-      Helmholtz(const double k = 0, const double alpha = 1)
-          : k(k), alpha(alpha){};
-      cmplx operator()(const Eigen::Vector3d &dr) const
-      {
-        const double R = dr.norm();
-        return std::exp(-iu * k * R) / (alpha * R);
-      }
-
-     private:
-      double k, alpha;
-    };
-  }
 }
 
 class AIM::AimInteraction final : public HistoryInteraction {
---
+++ src/interactions/AIM/normalization.h
@@ -0,0 +1,38 @@
+#ifndef NORMALIZATION_H
+#define NORMALIZATION_H
+
+namespace AIM {
+  namespace normalization {
+    using SpatialNorm = std::function<cmplx(const Eigen::Vector3d &)>;
+    const SpatialNorm unit = [](__attribute__((unused))
+                                const Eigen::Vector3d &v) { return 1; };
+
+    class Laplace {
+     public:
+      Laplace(const double alpha = 1) : alpha(alpha){};
+      double operator()(const Eigen::Vector3d &dr) const
+      {
+        return 1 / (alpha * dr.norm());
+      }
+
+     private:
+      double alpha;
+    };
+
+    class Helmholtz {
+     public:
+      Helmholtz(const double k = 0, const double alpha = 1)
+          : k(k), alpha(alpha){};
+      cmplx operator()(const Eigen::Vector3d &dr) const
+      {
+        const double R = dr.norm();
+        return std::exp(-iu * k * R) / (alpha * R);
+      }
+
+     private:
+      double k, alpha;
+    };
+  }
+}
+
+#endif
