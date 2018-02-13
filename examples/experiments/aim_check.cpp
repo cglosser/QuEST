674a6feeb22d79ec225d43ab1fc5a93dc26ca042
Connor Glosser <glosser1@gmail.com>
2018-02-12 21:21:01 -0500

    Checking imaginary-channel propagation
    Seems to have an error...

--- examples/experiments/aim_check.cpp
+++ examples/experiments/aim_check.cpp
@@ -19,15 +19,15 @@ class Gaussian {
 
 int main()
 {
-  const int interpolation_order = 4, expansion_order = 3, num_steps = 2048;
-  const double c = 1, dt = 0.5, total_time = num_steps * dt, omega = 0;
+  const int interpolation_order = 4, expansion_order = 1, num_steps = 1024;
+  const double c = 1, dt = 1, total_time = num_steps * dt, omega = 0;
   const Gaussian source(total_time / 2.0, total_time / 12.0);
 
-  const Eigen::Array3d spacing(1.6, 1.6, 1.6);
+  const Eigen::Array3d spacing(1, 1, 1);
 
   auto dots = std::make_shared<DotVector>();
-  dots->push_back(QuantumDot({0.0, 0.0, 0.0}, omega, {0, 0}, {0, 0, 1}));
-  dots->push_back(QuantumDot({0.0, 0.0, 10.0}, omega, {0, 0}, {0, 0, 1}));
+  dots->push_back(QuantumDot({0.5, 0.5, 0.5}, omega, {0, 0}, {0, 0, 1}));
+  dots->push_back(QuantumDot({0.5, 0.5, 10.5}, omega, {0, 0}, {0, 0, 1}));
 
   const int num_dots = dots->size();
   auto history = std::make_shared<Integrator::History<Eigen::Vector2cd>>(
@@ -38,22 +38,32 @@ int main()
     }
   }
 
-  Propagation::Laplace<cmplx> laplace;
+  //Propagation::RotatingEFIE efie(c, 1, omega);
+  //int nt = AIM::Grid(spacing, expansion_order, *dots).max_transit_steps(c, dt) +
+           //interpolation_order;
+
+  //AIM::Interaction aim(dots, history, efie, spacing, interpolation_order,
+                       //expansion_order, 1, c, dt,
+                       //AIM::Expansions::RotatingEFIE(nt, c, dt, omega),
+                       //AIM::normalization::Helmholtz(omega / c));
+  //DirectInteraction direct(dots, history, efie, interpolation_order, c, dt);
+
+  Propagation::Identity<cmplx> identity;
   int nt = AIM::Grid(spacing, expansion_order, *dots).max_transit_steps(c, dt) +
            interpolation_order;
 
-  AIM::Interaction aim(
-      dots, history, laplace, spacing, interpolation_order, expansion_order, 1,
-      c, dt, AIM::Expansions::Retardation(nt), AIM::normalization::Laplace());
-  DirectInteraction direct(dots, history, laplace, interpolation_order, c, dt);
+  AIM::Interaction aim(dots, history, identity, spacing, interpolation_order,
+                       expansion_order, 1, c, dt,
+                       AIM::Expansions::Retardation(nt),
+                       AIM::normalization::unit);
+  DirectInteraction direct(dots, history, identity, interpolation_order, c, dt);
 
   std::ofstream comparison("comparison.dat");
   comparison.precision(17);
   for(int t = 0; t < num_steps; ++t) {
-    std::cout << t << std::endl;
-    comparison << t * dt << " "
-               << (direct.evaluate(t) - aim.evaluate(t)).matrix().norm()
-               << std::endl;
+    std::cout << t << " ";
+    comparison << t * dt << " " << direct.evaluate(t).transpose() << " "
+               << aim.evaluate(t).transpose() << std::endl;
   }
 
   return 0;
