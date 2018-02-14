71f5b045761e862a16eb04a83bb0b726c70bae01
Connor Glosser <glosser1@gmail.com>
2018-02-13 20:39:00 -0500

    Idk, check stuff? I'm tired of fixing this.
    SO TIRED OF FIXING THIS.

--- examples/experiments/aim_check.cpp
+++ examples/experiments/aim_check.cpp
@@ -19,44 +19,34 @@ class Gaussian {
 
 int main()
 {
-  const int interpolation_order = 4, expansion_order = 1, num_steps = 1024;
-  const double c = 1, dt = 1, total_time = num_steps * dt, omega = 0;
+  const int interpolation_order = 4, expansion_order = 5, num_steps = 1024;
+  const double c = 1, dt = 1, total_time = num_steps * dt, omega = 0.1;
   const Gaussian source(total_time / 2.0, total_time / 12.0);
 
   const Eigen::Array3d spacing(1, 1, 1);
 
   auto dots = std::make_shared<DotVector>();
   dots->push_back(QuantumDot({0.5, 0.5, 0.5}, omega, {0, 0}, {0, 0, 1}));
-  dots->push_back(QuantumDot({0.5, 0.5, 10.5}, omega, {0, 0}, {0, 0, 1}));
+  dots->push_back(QuantumDot({0.5, 0.7, 10.5}, omega, {0, 0}, {0, 0, 1}));
 
   const int num_dots = dots->size();
   auto history = std::make_shared<Integrator::History<Eigen::Vector2cd>>(
       num_dots, 10, num_steps);
-  for(int d = 0; d < num_dots; ++d) {
-    for(int t = -10; t < num_steps; ++t) {
-      history->array_[d][t][0](RHO_01) = 2 * source(2 * t * dt) + iu * source(t * dt);
-    }
+  history->fill(Eigen::Vector2cd::Zero());
+  for(int t = -10; t < num_steps; ++t) {
+    history->array_[0][t][0](RHO_01) =
+        2 * source(2 * t * dt) + iu * source(t * dt);
   }
 
-  //Propagation::RotatingEFIE efie(c, 1, omega);
-  //int nt = AIM::Grid(spacing, expansion_order, *dots).max_transit_steps(c, dt) +
-           //interpolation_order;
-
-  //AIM::Interaction aim(dots, history, efie, spacing, interpolation_order,
-                       //expansion_order, 1, c, dt,
-                       //AIM::Expansions::RotatingEFIE(nt, c, dt, omega),
-                       //AIM::normalization::Helmholtz(omega / c));
-  //DirectInteraction direct(dots, history, efie, interpolation_order, c, dt);
-
-  Propagation::Identity<cmplx> identity;
+  Propagation::RotatingEFIE prop(c, 1, omega);
   int nt = AIM::Grid(spacing, expansion_order, *dots).max_transit_steps(c, dt) +
            interpolation_order;
 
-  AIM::Interaction aim(dots, history, identity, spacing, interpolation_order,
+  AIM::Interaction aim(dots, history, prop, spacing, interpolation_order,
                        expansion_order, 1, c, dt,
-                       AIM::Expansions::Retardation(nt),
-                       AIM::normalization::unit);
-  DirectInteraction direct(dots, history, identity, interpolation_order, c, dt);
+                       AIM::Expansions::RotatingEFIE(nt, c, dt, omega),
+                       AIM::normalization::Helmholtz(omega / c, 1));
+  DirectInteraction direct(dots, history, prop, interpolation_order, c, dt);
 
   std::ofstream comparison("comparison.dat");
   comparison.precision(17);
--- examples/experiments/multiphysics.cpp
+++ examples/experiments/multiphysics.cpp
@@ -19,47 +19,50 @@ class Gaussian {
 
 int main()
 {
-  const int interpolation_order = 4, expansion_order = 4, num_steps = 4000;
-  const double c = 299.792458, dt = 0.5e-2, total_time = 20, omega = 2278.9013;
+  const int interpolation_order = 4, expansion_order = 5, num_steps = 1024;
+  const double c = 299.792458, dt = 4.136e-2, total_time = dt * num_steps,
+               omega = 2278.9013;
   const Gaussian source(total_time / 2.0, total_time / 12.0);
 
-  const Eigen::Array3d spacing(0.10, 0.10, 0.10);
+  const double h = 2 * M_PI * c / (20 * omega);
+  const Eigen::Array3d spacing(h, h, h);
 
   auto dots = std::make_shared<DotVector>();
-  dots->push_back(QuantumDot(
-      {-0.006772025629369738, 0.006193253263435561, -0.010766531732233575},
-      2278.9013, {10, 20}, {0.00052917721, 0, 0}));
-  dots->push_back(QuantumDot(
-      {0.011823480098434758, 0.010089453212510147, -0.008611542585243048},
-      2278.9013, {10, 20}, {0.00052917721, 0, 0}));
+  dots->push_back(
+      QuantumDot({0, 0, 0}, 2278.9013, {10, 20}, {0.00052917721, 0, 0}));
+  dots->push_back(QuantumDot({h / 2, h / 2, h / 2}, 2278.9013, {10, 20},
+                             {0.00052917721, 0, 0}));
+
+  // std::cout << omega << std::endl;
+  // std::cout << h << std::endl;
+  // std::cout << (dots->at(1).position() - dots->at(0).position()).norm()
+  //<< std::endl;
+  // std::cout << dt << std::endl;
+  // std::cout << total_time << std::endl;
 
   const int num_dots = dots->size();
   auto history = std::make_shared<Integrator::History<Eigen::Vector2cd>>(
       num_dots, 10, num_steps);
-  for(int d = 0; d < num_dots; ++d) {
-    for(int t = -10; t < num_steps; ++t) {
-      history->array_[d][t][0](RHO_01) = source(t * dt);
-    }
+  for(int t = -10; t < num_steps; ++t) {
+    history->array_[0][t][0](RHO_01) = source(t * dt);
   }
 
-  Propagation::RotatingEFIE efie(c, 2.4341349e-5, omega);
-  int nt = AIM::Grid(spacing, expansion_order, *dots).max_transit_steps(c, dt) +
-           interpolation_order;
+  Propagation::EFIE<cmplx> prop(c, 2.4341349e-5);
+  auto grid = AIM::Grid(spacing, expansion_order, *dots);
+  int nt = grid.max_transit_steps(c, dt) + interpolation_order;
 
-  AIM::Interaction aim(dots, history, efie, spacing, interpolation_order,
+  AIM::Interaction aim(dots, history, prop, spacing, interpolation_order,
                        expansion_order, 1, c, dt,
-                       AIM::Expansions::RotatingEFIE(nt, c, dt, omega),
-                       AIM::normalization::Helmholtz(omega / c, 2.4341349e-5));
-  DirectInteraction direct(dots, history, efie, interpolation_order, c, dt);
+                       AIM::Expansions::EFIE(nt, c, dt),
+                       AIM::normalization::Laplace(2.4341349e-5));
+  DirectInteraction direct(dots, history, prop, interpolation_order, c, dt);
 
   std::ofstream comparison("comparison.dat");
   comparison.precision(17);
   for(int t = 0; t < num_steps; ++t) {
-    std::cout << t << std::endl;
-    // comparison << t * dt << " "
-    //<< (direct.evaluate(t) - aim.evaluate(t)).matrix().norm()
-    //<< std::endl;
-    comparison << direct.evaluate(t).transpose() << "     " << aim.evaluate(t).transpose() << std::endl;
+    if(t % 100 == 0) std::cout << t << std::endl;
+    comparison << direct.evaluate(t).transpose() << " "
+               << aim.evaluate(t).transpose() << "\n";
   }
 
   return 0;
