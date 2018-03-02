e9611deb008f84999d43b5c74f4f3ed2da154a8b
Connor Glosser <glosser1@gmail.com>
2018-03-02 10:28:16 -0500

    Initialize Results to zero

--- src/interactions/interaction.h
+++ src/interactions/interaction.h
@@ -11,7 +11,10 @@ class InteractionBase {
   typedef Eigen::Array<cmplx, Eigen::Dynamic, 1> ResultArray;
 
   InteractionBase(const std::shared_ptr<const DotVector> dots, const double dt)
-      : dots(std::move(dots)), results(dots ? dots->size() : 0), dt(dt){};
+      : dots(std::move(dots)),
+        results{Eigen::Array<cmplx, Eigen::Dynamic, 1>::Zero(
+            dots ? dots->size() : 0, 1)},
+        dt(dt){};
   const cmplx &operator[](const int i) const { return results[i]; }
   virtual const ResultArray &evaluate(const int) = 0;
   virtual ~InteractionBase(){};
