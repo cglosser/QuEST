7d9ee0f7c0c1c5cf0ede1dee2f233f3d74524b60
Connor Glosser <glosser1@gmail.com>
2018-10-29 22:10:20 -0400

    Fix up get eigen script

--- install/get_eigen.sh
+++ install/get_eigen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://bitbucket.org/eigen/eigen/get/3.3.2.tar.gz -O /tmp/eigen.tar.gz
-tar -xvzf /tmp/eigen.tar.gz
+cd /tmp
+wget https://bitbucket.org/eigen/eigen/get/3.3.2.tar.gz -O eigen.tar.gz
 mkdir -p eigen/build && tar -xvzf eigen.tar.gz -C eigen --strip-components 1
 cd eigen/build && cmake .. && make install
