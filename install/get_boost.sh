eda915da551bcd2eb7cbc5644e6c9fa162e95b71
Connor Glosser <glosser1@gmail.com>
2018-11-02 15:27:01 -0400

    Debugging get boost

--- install/get_boost.sh
+++ install/get_boost.sh
@@ -2,6 +2,7 @@
 cd /tmp
 wget https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.bz2 -O boost.tar.bz2
 mkdir -p boost && tar -xvf boost.tar.bz2 -C boost --strip-components 1
+ls
 cd boost/
 ./bootstrap --with-libraries=program_options,test
 ./b2 install
