28c92a0b703ecd8d4d266871a80c804a1acb8b54
Connor Glosser <glosser1@gmail.com>
2018-11-02 15:37:25 -0400

    changes

--- install/get_boost.sh
+++ install/get_boost.sh
@@ -4,7 +4,7 @@ cd /tmp
 
 echo "Downloading and unpacking boost"
 wget https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.bz2 -O boost.tar.bz2
-mkdir -p boost && tar -xvf boost.tar.bz2 -C boost --strip-components 1
+mkdir -p boost && tar -xvf boost.tar.bz2 -C boost --strip-components 1 > /dev/null
 ls
 
 echo "Building boost"
