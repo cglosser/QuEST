32577f81c6d24580c96edfceb6df064848e70351
Connor Glosser <glosser1@gmail.com>
2018-11-02 15:31:29 -0400

    well crap.

--- install/get_boost.sh
+++ install/get_boost.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+echo "Changing to tmp"
 cd /tmp
+
+echo "Downloading and unpacking boost"
 wget https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.bz2 -O boost.tar.bz2
 mkdir -p boost && tar -xvf boost.tar.bz2 -C boost --strip-components 1
 ls
+
+echo "Building boost"
 cd boost/
 ./bootstrap --with-libraries=program_options,test
 ./b2 install
